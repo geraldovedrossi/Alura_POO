47bdeb3d676d35437cdb5ca3d45c1c2a346c3c27
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-05-17 17:28:50 -0300

    Dia 2 - Aulas 3 e 4

---
+++ src/bytebank/CriaConta.java
@@ -0,0 +1,46 @@
+package bytebank;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		
+		
+		Cliente ge = new Cliente ("Geraldo", "666666","Programador");
+		Conta contaDoGe = new Conta(0, 0);
+		
+
+//		Conta contaDaMa = new Conta();
+//		
+//		Cliente ge = new Cliente();
+//		ge.nome = "Geraldo Neto";
+//		ge.cpf = "222.222.222-22";
+//		ge.profissao = "Programador";
+//		
+//		Cliente ma = new Cliente();
+//		ma.nome = "Marcela Almeida";
+//		ma.cpf = "222.222.222-23";
+//		ma.profissao = "Programadora";
+//		
+//		contaDoGe.titular = ge;
+//		contaDaMa.titular = ma;
+//		
+//		contaDoGe.deposita(250);
+//		
+//		System.out.printf("O saldo da primeira conta é de: R$%.2f%n", contaDoGe.saldo);
+//		
+//		contaDoGe.saca(50);
+//		
+//		System.out.printf("O saldo da primeira conta é de: R$%.2f%n", contaDoGe.saldo);
+//		System.out.printf("O saldo da segunda conta é de: R$%.2f%n", contaDaMa.saldo);
+//
+//		contaDoGe.tranfere(100, contaDaMa);
+//		
+//		System.out.printf("O saldo da primeira conta é de: R$%.2f%n", contaDoGe.saldo);
+//		System.out.printf("O saldo da segunda conta é de: R$%.2f%n", contaDaMa.saldo);
+//		
+//		System.out.println(contaDoGe.titular.nome);
+		
+		
+	}
+
+}
